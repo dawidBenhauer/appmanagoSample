cebd1c341f658bd8864bbc0c3c55ef1ad614d9bd
Dawid Beer <dawid.beer@salesmanago.pl>
2016-08-05 11:19:21 +0200

    Add buttons and textbox to register mail

--- app/src/main/java/com/example/dawid/androidmanual/MainActivity.java
+++ app/src/main/java/com/example/dawid/androidmanual/MainActivity.java
@@ -1,21 +1,12 @@
 package com.example.dawid.androidmanual;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.os.Handler;
-import android.preference.PreferenceManager;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.appmanago.exception.NotFoundRequiredPropertiesException;
@@ -24,14 +15,18 @@ import com.appmanago.lib.AmMonitor;
 import com.appmanago.lib.AmMonitoring;
 import com.appmanago.lib.AmProperties;
 import com.appmanago.lib.gcm.AmRegistrationIntentService;
-import com.appmanago.lib.helper.PreferencesGateway;
 import com.appmanago.model.Constants;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     private AmMonitoring amMonitor;
+    private Button button;
+    private Button buttonReg;
+    EditText emailInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +46,34 @@ public class MainActivity extends AppCompatActivity {
 
         handleAppmanagoExtras(getIntent().getExtras());
 
-        amMonitor.syncEmail("SampleUser@o2.pl");
+        //amMonitor.syncEmail("SampleUser@o2.pl");
 
+        button = (Button) findViewById(R.id.button);
+        buttonReg = (Button) findViewById(R.id.buttonReg);
+        emailInput = (EditText) findViewById(R.id.sync_email_input);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AmProperties properties = new AmProperties();
+                properties.addProperty("promo", true);
+                properties.addProperty("date", new Date());
+                amMonitor.clicked("samplebutton", properties);
+
+            }
+        });
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AmProperties properties = new AmProperties();
+                properties.addProperty("promo", true);
+                properties.addProperty("date", new Date());
+                amMonitor.syncEmail(emailInput.getText().toString());
+                amMonitor.clicked("registationButton", properties);
+
+            }
+        });
     }
 
     @Override
